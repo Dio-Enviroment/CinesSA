607dceab47ab49aeb885a0d653db80ae8c7802f0
Dio-Enviroment <dio.german93@gmail.com>
2020-05-30 14:38:52 -0500

    z

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -38,7 +38,7 @@ public class Main extends JFrame {
 	 * Create the frame.
 	 */
 	public Main() {
-		System.out.println("x13");
+		System.out.println("x14");
 		contentPane = new JPanel();
 		ControladorPrincipal controladorPrincipal = new ControladorPrincipal(this);
 		ControladorView controladorView = controladorPrincipal.getCtrView();
