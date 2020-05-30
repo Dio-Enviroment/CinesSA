a5601d866c1a369a39c819d49f974fbb123e083b
kevin <kevin@LAPTOP-EPM4VVHH>
2020-05-30 15:28:52 -0500

    ñññññ

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -46,6 +46,7 @@ public class Main extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1024, 768);
 		System.out.println("Ha");
+		System.out.println("z");
 		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
