ec6ba123d56b3130e0ba7def4c943d10b99e74fd
Dio-Enviroment <dio.german93@gmail.com>
2020-05-30 16:40:59 -0500

    nuevos

--- src/modelo/Conferencia.java
+++ src/modelo/Conferencia.java
@@ -3,11 +3,13 @@ package modelo;
 public class Conferencia extends Proyeccion {
 	private String presentador;
 	private String tematica;
+	private String salaTipo;
 
-	public Conferencia(String boleteria_back,String boleteria_front,String boleteria_title, String titulo, String duracion, String horario, String presentador,String tematica) {
+	public Conferencia(String boleteria_back,String boleteria_front,String boleteria_title, String titulo, String duracion, String horario, String presentador,String tematica,String salaTipo) {
 		super(boleteria_back,boleteria_front,boleteria_title,titulo,duracion,horario);
 		this.presentador = presentador;
 		this.tematica = tematica;
+		this.salaTipo=salaTipo;
 	}
 
 	public String getPresentador() {
@@ -25,5 +27,11 @@ public class Conferencia extends Proyeccion {
 	public void setTematica(String tematica) {
 		this.tematica = tematica;
 	}
-	
+	public String getSalaTipo() {
+		return salaTipo;
+	}
+
+	public void setSalaTipo(String salaTipo) {
+		this.salaTipo = salaTipo;
+	}
 }
