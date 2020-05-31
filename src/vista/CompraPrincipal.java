c5005ca46f628ab37c2e442b7adcbcf7fe4ae8d6
Dio-Enviroment <dio.german93@gmail.com>
2020-05-31 01:51:26 -0500

    nuevo121

--- src/vista/CompraPrincipal.java
+++ src/vista/CompraPrincipal.java
@@ -77,10 +77,14 @@ public class CompraPrincipal extends CustomPanel{
 	private ControladorSala ctrSala;
 	private Precio pr=new Precio();
 
-	/**
-	 * Create the panel.
-	 */
-
+	private GridBagConstraints gbc_lbl_cedula,
+	gbc_txt_cedula,gbc_lbl_nombre,gbc_txt_nombre,
+	gbc_lbl_telefono,gbc_txt_fono,gbc_lbl_direccion,
+	gbc_txt_direccion,gbc_lbl_pelicula,gbc_lbl_tituloPelicula,
+	gbc_lbl_tiiposala,gbc_lbl_boletos,gbc_lbl_sp,gbc_lbl_ninios,
+	gbc_sp_ninios,gbc_lbl_adultos,gbc_sp_adultos,gbc_btn_selecionAsiento,
+	gbc_lbl_contador,gbc_lbl_subtotal,gbc_lbl_valor_Subtotal,
+	gbc_lbl_iva,gbc_lbl_valor_iva,gbc_lbl_total,gbc_lbl_valor_total;
 	
 
 	public CompraPrincipal(int width, int height, ControladorProyeccion ctrProyeccion,ControladorSala ctrSala,  ControladorView ctrView) {
@@ -92,12 +96,12 @@ public class CompraPrincipal extends CustomPanel{
 
 		pn_compra = new JPanel();
 		pn_compra.setBounds(12, 13, 513, 526);
-		add(pn_compra);
+		//add(pn_compra);
 		pn_compra.setLayout(null);
 
 		pn_datoPersona = new JPanel();
 		pn_datoPersona.setBounds(12, 23, 494, 182);
-		pn_compra.add(pn_datoPersona);
+		//pn_compra.add(pn_datoPersona);
 		pn_datoPersona.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0), 2, true),
 				"INFORMACI\u00D3N DEL CLIENTE", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		GridBagLayout gbl_pn_datoPersona = new GridBagLayout();
@@ -110,83 +114,83 @@ public class CompraPrincipal extends CustomPanel{
 		lbl_cedula = new JLabel("C\u00C9DULA:");
 		lbl_cedula.setForeground(new Color(102, 0, 102));
 		lbl_cedula.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_cedula = new GridBagConstraints();
+		 gbc_lbl_cedula = new GridBagConstraints();
 		gbc_lbl_cedula.anchor = GridBagConstraints.EAST;
 		gbc_lbl_cedula.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_cedula.gridx = 1;
 		gbc_lbl_cedula.gridy = 1;
-		pn_datoPersona.add(lbl_cedula, gbc_lbl_cedula);
+		//pn_datoPersona.add(lbl_cedula, gbc_lbl_cedula);
 
 		txt_cedula = new JTextField();
 
-		GridBagConstraints gbc_txt_cedula = new GridBagConstraints();
+		 gbc_txt_cedula = new GridBagConstraints();
 		gbc_txt_cedula.anchor = GridBagConstraints.WEST;
 		gbc_txt_cedula.insets = new Insets(0, 0, 5, 0);
 		gbc_txt_cedula.gridx = 2;
 		gbc_txt_cedula.gridy = 1;
-		pn_datoPersona.add(txt_cedula, gbc_txt_cedula);
+		//pn_datoPersona.add(txt_cedula, gbc_txt_cedula);
 		txt_cedula.setColumns(10);
 
 		lbl_nombre = new JLabel("NOMBRE:");
 		lbl_nombre.setForeground(new Color(102, 0, 102));
 		lbl_nombre.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_nombre = new GridBagConstraints();
+		 gbc_lbl_nombre = new GridBagConstraints();
 		gbc_lbl_nombre.anchor = GridBagConstraints.EAST;
 		gbc_lbl_nombre.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_nombre.gridx = 1;
 		gbc_lbl_nombre.gridy = 2;
-		pn_datoPersona.add(lbl_nombre, gbc_lbl_nombre);
+		//pn_datoPersona.add(lbl_nombre, gbc_lbl_nombre);
 
 		txt_nombre = new JTextField();
-		GridBagConstraints gbc_txt_nombre = new GridBagConstraints();
+		 gbc_txt_nombre = new GridBagConstraints();
 		gbc_txt_nombre.insets = new Insets(0, 0, 5, 0);
 		gbc_txt_nombre.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txt_nombre.gridx = 2;
 		gbc_txt_nombre.gridy = 2;
-		pn_datoPersona.add(txt_nombre, gbc_txt_nombre);
+		//pn_datoPersona.add(txt_nombre, gbc_txt_nombre);
 		txt_nombre.setColumns(10);
 
 		lbl_telefono = new JLabel("TELEFONO:");
 		lbl_telefono.setForeground(new Color(102, 0, 102));
 		lbl_telefono.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_telefono = new GridBagConstraints();
+		 gbc_lbl_telefono = new GridBagConstraints();
 		gbc_lbl_telefono.anchor = GridBagConstraints.EAST;
 		gbc_lbl_telefono.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_telefono.gridx = 1;
 		gbc_lbl_telefono.gridy = 3;
-		pn_datoPersona.add(lbl_telefono, gbc_lbl_telefono);
+		//pn_datoPersona.add(lbl_telefono, gbc_lbl_telefono);
 
 		txt_fono = new JTextField();
-		GridBagConstraints gbc_txt_fono = new GridBagConstraints();
+		 gbc_txt_fono = new GridBagConstraints();
 		gbc_txt_fono.anchor = GridBagConstraints.NORTHWEST;
 		gbc_txt_fono.insets = new Insets(0, 0, 5, 0);
 		gbc_txt_fono.gridx = 2;
 		gbc_txt_fono.gridy = 3;
-		pn_datoPersona.add(txt_fono, gbc_txt_fono);
+		//pn_datoPersona.add(txt_fono, gbc_txt_fono);
 		txt_fono.setColumns(10);
 
 		lbl_direccion = new JLabel("DIRECCION:");
 		lbl_direccion.setForeground(new Color(102, 0, 102));
 		lbl_direccion.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_direccion = new GridBagConstraints();
+		 gbc_lbl_direccion = new GridBagConstraints();
 		gbc_lbl_direccion.anchor = GridBagConstraints.EAST;
 		gbc_lbl_direccion.insets = new Insets(0, 0, 0, 5);
 		gbc_lbl_direccion.gridx = 1;
 		gbc_lbl_direccion.gridy = 4;
-		pn_datoPersona.add(lbl_direccion, gbc_lbl_direccion);
+		//pn_datoPersona.add(lbl_direccion, gbc_lbl_direccion);
 
 		txt_direccion = new JTextField();
 		txt_direccion.setText("");
-		GridBagConstraints gbc_txt_direccion = new GridBagConstraints();
+		 gbc_txt_direccion = new GridBagConstraints();
 		gbc_txt_direccion.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txt_direccion.gridx = 2;
 		gbc_txt_direccion.gridy = 4;
-		pn_datoPersona.add(txt_direccion, gbc_txt_direccion);
+		//pn_datoPersona.add(txt_direccion, gbc_txt_direccion);
 		txt_direccion.setColumns(10);
 
 		pn_datoBoleto = new JPanel();
 		pn_datoBoleto.setBounds(12, 205, 494, 270);
-		pn_compra.add(pn_datoBoleto);
+		//pn_compra.add(pn_datoBoleto);
 		pn_datoBoleto.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0), 2), "DATOS DEL BOLETO",
 				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		GridBagLayout gbl_pn_datoBoleto = new GridBagLayout();
@@ -199,171 +203,171 @@ public class CompraPrincipal extends CustomPanel{
 		lbl_pelicula = new JLabel("PEL\u00CDCULA:");
 		lbl_pelicula.setForeground(new Color(102, 0, 102));
 		lbl_pelicula.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_pelicula = new GridBagConstraints();
+		 gbc_lbl_pelicula = new GridBagConstraints();
 		gbc_lbl_pelicula.anchor = GridBagConstraints.EAST;
 		gbc_lbl_pelicula.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_pelicula.gridx = 1;
 		gbc_lbl_pelicula.gridy = 1;
-		pn_datoBoleto.add(lbl_pelicula, gbc_lbl_pelicula);
+		//pn_datoBoleto.add(lbl_pelicula, gbc_lbl_pelicula);
 
 		lbl_tituloPelicula = new JLabel("titulo de la pelicula");
 		lbl_tituloPelicula.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_tituloPelicula = new GridBagConstraints();
+		 gbc_lbl_tituloPelicula = new GridBagConstraints();
 		gbc_lbl_tituloPelicula.anchor = GridBagConstraints.WEST;
 		gbc_lbl_tituloPelicula.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_tituloPelicula.gridx = 2;
 		gbc_lbl_tituloPelicula.gridy = 1;
-		pn_datoBoleto.add(lbl_tituloPelicula, gbc_lbl_tituloPelicula);
+		//pn_datoBoleto.add(lbl_tituloPelicula, gbc_lbl_tituloPelicula);
 
 		lbl_tiiposala = new JLabel("tipo de sala");
 		lbl_tiiposala.setHorizontalAlignment(SwingConstants.TRAILING);
 		lbl_tiiposala.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_tiiposala = new GridBagConstraints();
+		 gbc_lbl_tiiposala = new GridBagConstraints();
 		gbc_lbl_tiiposala.insets = new Insets(0, 0, 5, 0);
 		gbc_lbl_tiiposala.gridx = 4;
 		gbc_lbl_tiiposala.gridy = 1;
-		pn_datoBoleto.add(lbl_tiiposala, gbc_lbl_tiiposala);
+		//pn_datoBoleto.add(lbl_tiiposala, gbc_lbl_tiiposala);
 
 		lbl_boletos = new JLabel("BOLETOS");
 		lbl_boletos.setForeground(new Color(102, 0, 102));
 		lbl_boletos.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_boletos = new GridBagConstraints();
+		 gbc_lbl_boletos = new GridBagConstraints();
 		gbc_lbl_boletos.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_boletos.gridx = 2;
 		gbc_lbl_boletos.gridy = 2;
-		pn_datoBoleto.add(lbl_boletos, gbc_lbl_boletos);
+		//pn_datoBoleto.add(lbl_boletos, gbc_lbl_boletos);
 
 		lbl_sp = new JLabel("");
-		GridBagConstraints gbc_lbl_sp = new GridBagConstraints();
+		 gbc_lbl_sp = new GridBagConstraints();
 		gbc_lbl_sp.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_sp.gridx = 0;
 		gbc_lbl_sp.gridy = 3;
-		pn_datoBoleto.add(lbl_sp, gbc_lbl_sp);
+		//pn_datoBoleto.add(lbl_sp, gbc_lbl_sp);
 
 		lbl_ninios = new JLabel("NI\u00D1OS:");
 		lbl_ninios.setForeground(new Color(102, 0, 102));
 		lbl_ninios.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_ninios = new GridBagConstraints();
+		 gbc_lbl_ninios = new GridBagConstraints();
 		gbc_lbl_ninios.anchor = GridBagConstraints.EAST;
 		gbc_lbl_ninios.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_ninios.gridx = 1;
 		gbc_lbl_ninios.gridy = 3;
-		pn_datoBoleto.add(lbl_ninios, gbc_lbl_ninios);
-		GridBagConstraints gbc_sp_ninios = new GridBagConstraints();
+		//pn_datoBoleto.add(lbl_ninios, gbc_lbl_ninios);
+		 gbc_sp_ninios = new GridBagConstraints();
 		gbc_sp_ninios.anchor = GridBagConstraints.WEST;
 		gbc_sp_ninios.insets = new Insets(0, 0, 5, 5);
 		gbc_sp_ninios.gridx = 2;
 		gbc_sp_ninios.gridy = 3;
-		pn_datoBoleto.add(sp_ninios, gbc_sp_ninios);
+		//pn_datoBoleto.add(sp_ninios, gbc_sp_ninios);
 
 		lbl_adultos = new JLabel("ADULTOS:");
 
 		lbl_adultos.setForeground(new Color(102, 0, 102));
 		lbl_adultos.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_adultos = new GridBagConstraints();
+		 gbc_lbl_adultos = new GridBagConstraints();
 		gbc_lbl_adultos.anchor = GridBagConstraints.EAST;
 		gbc_lbl_adultos.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_adultos.gridx = 1;
 		gbc_lbl_adultos.gridy = 4;
-		pn_datoBoleto.add(lbl_adultos, gbc_lbl_adultos);
-		GridBagConstraints gbc_sp_adultos = new GridBagConstraints();
+		//pn_datoBoleto.add(lbl_adultos, gbc_lbl_adultos);
+		 gbc_sp_adultos = new GridBagConstraints();
 		gbc_sp_adultos.anchor = GridBagConstraints.WEST;
 		gbc_sp_adultos.insets = new Insets(0, 0, 5, 5);
 		gbc_sp_adultos.gridx = 2;
 		gbc_sp_adultos.gridy = 4;
-		pn_datoBoleto.add(sp_adultos, gbc_sp_adultos);
+		//pn_datoBoleto.add(sp_adultos, gbc_sp_adultos);
 		
 				btn_selecionAsiento = new JButton("SELEC ASIENTOS");
 				btn_selecionAsiento.setFont(new Font("Verdana", Font.BOLD, 13));
 				btn_selecionAsiento.setForeground(new Color(102, 0, 102));
-				GridBagConstraints gbc_btn_selecionAsiento = new GridBagConstraints();
+				 gbc_btn_selecionAsiento = new GridBagConstraints();
 				gbc_btn_selecionAsiento.insets = new Insets(0, 0, 5, 5);
 				gbc_btn_selecionAsiento.gridx = 1;
 				gbc_btn_selecionAsiento.gridy = 5;
-				pn_datoBoleto.add(btn_selecionAsiento, gbc_btn_selecionAsiento);
+				//pn_datoBoleto.add(btn_selecionAsiento, gbc_btn_selecionAsiento);
 				this.btn_selecionAsiento.setEnabled(false);
 		
 				lbl_contador = new JLabel("0");
 				lbl_contador.setFont(new Font("Verdana", Font.BOLD, 14));
-				GridBagConstraints gbc_lbl_contador = new GridBagConstraints();
+				 gbc_lbl_contador = new GridBagConstraints();
 				gbc_lbl_contador.anchor = GridBagConstraints.WEST;
 				gbc_lbl_contador.insets = new Insets(0, 0, 5, 5);
 				gbc_lbl_contador.gridx = 2;
 				gbc_lbl_contador.gridy = 5;
-				pn_datoBoleto.add(lbl_contador, gbc_lbl_contador);
+				//pn_datoBoleto.add(lbl_contador, gbc_lbl_contador);
 
 		lbl_subtotal = new JLabel("SUBTOTAL:");
 		lbl_subtotal.setForeground(new Color(102, 0, 102));
 		lbl_subtotal.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_subtotal = new GridBagConstraints();
+		 gbc_lbl_subtotal = new GridBagConstraints();
 		gbc_lbl_subtotal.anchor = GridBagConstraints.EAST;
 		gbc_lbl_subtotal.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_subtotal.gridx = 1;
 		gbc_lbl_subtotal.gridy = 6;
-		pn_datoBoleto.add(lbl_subtotal, gbc_lbl_subtotal);
+		//pn_datoBoleto.add(lbl_subtotal, gbc_lbl_subtotal);
 
 		lbl_valor_Subtotal = new JLabel("0");
 		lbl_valor_Subtotal.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_valor_Subtotal = new GridBagConstraints();
+		 gbc_lbl_valor_Subtotal = new GridBagConstraints();
 		gbc_lbl_valor_Subtotal.anchor = GridBagConstraints.WEST;
 		gbc_lbl_valor_Subtotal.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_valor_Subtotal.gridx = 2;
 		gbc_lbl_valor_Subtotal.gridy = 6;
-		pn_datoBoleto.add(lbl_valor_Subtotal, gbc_lbl_valor_Subtotal);
+		//pn_datoBoleto.add(lbl_valor_Subtotal, gbc_lbl_valor_Subtotal);
 
 		lbl_iva = new JLabel("IVA:");
 		lbl_iva.setForeground(new Color(102, 0, 102));
 		lbl_iva.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_iva = new GridBagConstraints();
+		 gbc_lbl_iva = new GridBagConstraints();
 		gbc_lbl_iva.anchor = GridBagConstraints.EAST;
 		gbc_lbl_iva.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_iva.gridx = 1;
 		gbc_lbl_iva.gridy = 7;
-		pn_datoBoleto.add(lbl_iva, gbc_lbl_iva);
+		//pn_datoBoleto.add(lbl_iva, gbc_lbl_iva);
 
 		lbl_valor_iva = new JLabel("0");
 		lbl_valor_iva.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_valor_iva = new GridBagConstraints();
+		 gbc_lbl_valor_iva = new GridBagConstraints();
 		gbc_lbl_valor_iva.anchor = GridBagConstraints.WEST;
 		gbc_lbl_valor_iva.insets = new Insets(0, 0, 5, 5);
 		gbc_lbl_valor_iva.gridx = 2;
 		gbc_lbl_valor_iva.gridy = 7;
-		pn_datoBoleto.add(lbl_valor_iva, gbc_lbl_valor_iva);
+		//pn_datoBoleto.add(lbl_valor_iva, gbc_lbl_valor_iva);
 
 		lbl_total = new JLabel("TOTAL:");
 		lbl_total.setForeground(new Color(102, 0, 102));
 		lbl_total.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_total = new GridBagConstraints();
+		 gbc_lbl_total = new GridBagConstraints();
 		gbc_lbl_total.anchor = GridBagConstraints.EAST;
 		gbc_lbl_total.insets = new Insets(0, 0, 0, 5);
 		gbc_lbl_total.gridx = 1;
 		gbc_lbl_total.gridy = 8;
-		pn_datoBoleto.add(lbl_total, gbc_lbl_total);
+		//pn_datoBoleto.add(lbl_total, gbc_lbl_total);
 
 		lbl_valor_total = new JLabel("0");
 		lbl_valor_total.setFont(new Font("Verdana", Font.BOLD, 14));
-		GridBagConstraints gbc_lbl_valor_total = new GridBagConstraints();
+		 gbc_lbl_valor_total = new GridBagConstraints();
 		gbc_lbl_valor_total.anchor = GridBagConstraints.WEST;
 		gbc_lbl_valor_total.insets = new Insets(0, 0, 0, 5);
 		gbc_lbl_valor_total.gridx = 2;
 		gbc_lbl_valor_total.gridy = 8;
-		pn_datoBoleto.add(lbl_valor_total, gbc_lbl_valor_total);
+		//pn_datoBoleto.add(lbl_valor_total, gbc_lbl_valor_total);
 
 		pn_controlBotones = new JPanel();
 		pn_controlBotones.setBounds(293, 473, 213, 40);
-		pn_compra.add(pn_controlBotones);
+		//pn_compra.add(pn_controlBotones);
 		pn_controlBotones.setBorder(new EmptyBorder(0, 0, 0, 0));
 		pn_controlBotones.setLayout(new BoxLayout(pn_controlBotones, BoxLayout.X_AXIS));
 
 		btn_anterior = new JButton("ANTERIOR");
 		btn_anterior.setForeground(new Color(102, 0, 102));
 		btn_anterior.setFont(new Font("Verdana", Font.BOLD, 13));
-		pn_controlBotones.add(btn_anterior);
+		//pn_controlBotones.add(btn_anterior);
 
 		btn_siguiente = new JButton("SIGUIENTE");
 		btn_siguiente.setForeground(new Color(102, 0, 102));
 		btn_siguiente.setFont(new Font("Verdana", Font.PLAIN, 13));
-		pn_controlBotones.add(btn_siguiente);
+		//pn_controlBotones.add(btn_siguiente);
 	
 		this.btn_siguiente.setEnabled(false);
 		//ra.a�adirSalas();
@@ -595,38 +599,70 @@ public class CompraPrincipal extends CustomPanel{
 
 	}
 	public void showcomponet(boolean visible){
-		 txt_cedula.setVisible(visible);
-		 txt_nombre.setVisible(visible);
-		 txt_fono.setVisible(visible);
-		 txt_direccion.setVisible(visible);
-		 lbl_pelicula.setVisible(visible);
-		 lbl_tituloPelicula.setVisible(visible);
-		 lbl_boletos.setVisible(visible);
-		 lbl_ninios.setVisible(visible);
-		 sp_ninios.setVisible(visible) ;
-		 lbl_adultos.setVisible(visible);
+		//  txt_cedula.setVisible(visible);
+		//  txt_nombre.setVisible(visible);
+		//  txt_fono.setVisible(visible);
+		//  txt_direccion.setVisible(visible);
+		//  lbl_pelicula.setVisible(visible);
+		//  lbl_tituloPelicula.setVisible(visible);
+		//  lbl_boletos.setVisible(visible);
+		//  lbl_ninios.setVisible(visible);
+		//  sp_ninios.setVisible(visible) ;
+		//  lbl_adultos.setVisible(visible);
 
-		 sp_adultos.setVisible(visible) ;
-		 btn_selecionAsiento.setVisible(visible);
-		 lbl_contador.setVisible(visible);
-		 lbl_subtotal.setVisible(visible);
-		 lbl_valor_Subtotal.setVisible(visible);
-		 lbl_iva.setVisible(visible);
-		 lbl_valor_iva.setVisible(visible);
-	     lbl_total.setVisible(visible);
-		 lbl_valor_total.setVisible(visible);
-	     pn_datoBoleto.setVisible(visible);
-		 pn_datoPersona.setVisible(visible);
-		 lbl_cedula.setVisible(visible);
-		 lbl_nombre.setVisible(visible);
-		 lbl_telefono.setVisible(visible);
-		 lbl_direccion.setVisible(visible);
-	     btn_anterior.setVisible(visible);
-		 btn_siguiente.setVisible(visible);
-		 pn_controlBotones.setVisible(visible);
-	     lbl_tiiposala.setVisible(visible);
-		 lbl_sp.setVisible(visible);
-		 pn_compra.setVisible(visible);
+		//  sp_adultos.setVisible(visible) ;
+		//  btn_selecionAsiento.setVisible(visible);
+		//  lbl_contador.setVisible(visible);
+		//  lbl_subtotal.setVisible(visible);
+		//  lbl_valor_Subtotal.setVisible(visible);
+		//  lbl_iva.setVisible(visible);
+		//  lbl_valor_iva.setVisible(visible);
+	    //  lbl_total.setVisible(visible);
+		//  lbl_valor_total.setVisible(visible);
+	    //  pn_datoBoleto.setVisible(visible);
+		//  pn_datoPersona.setVisible(visible);
+		//  lbl_cedula.setVisible(visible);
+		//  lbl_nombre.setVisible(visible);
+		//  lbl_telefono.setVisible(visible);
+		//  lbl_direccion.setVisible(visible);
+	    //  btn_anterior.setVisible(visible);
+		//  btn_siguiente.setVisible(visible);
+		//  pn_controlBotones.setVisible(visible);
+	    //  lbl_tiiposala.setVisible(visible);
+		//  lbl_sp.setVisible(visible);
+		//  pn_compra.setVisible(visible);
 		 //dfgdfgf
+
+		 add(pn_compra);
+		 pn_compra.add(pn_datoPersona);
+		 pn_datoPersona.add(lbl_cedula, gbc_lbl_cedula);
+		 pn_datoPersona.add(txt_cedula, gbc_txt_cedula);
+		 pn_datoPersona.add(lbl_nombre, gbc_lbl_nombre);
+		 pn_datoPersona.add(txt_nombre, gbc_txt_nombre);
+		 pn_datoPersona.add(lbl_telefono, gbc_lbl_telefono);
+		 pn_datoPersona.add(txt_fono, gbc_txt_fono);
+		 pn_datoPersona.add(lbl_direccion, gbc_lbl_direccion);
+		 pn_datoPersona.add(txt_direccion, gbc_txt_direccion);
+		 pn_compra.add(pn_datoBoleto);
+		 pn_datoBoleto.add(lbl_pelicula, gbc_lbl_pelicula);
+		 pn_datoBoleto.add(lbl_tituloPelicula, gbc_lbl_tituloPelicula);
+		 pn_datoBoleto.add(lbl_tiiposala, gbc_lbl_tiiposala);
+		 pn_datoBoleto.add(lbl_boletos, gbc_lbl_boletos);
+		 pn_datoBoleto.add(lbl_sp, gbc_lbl_sp);
+		 pn_datoBoleto.add(lbl_ninios, gbc_lbl_ninios);
+		 pn_datoBoleto.add(sp_ninios, gbc_sp_ninios);
+		 pn_datoBoleto.add(lbl_adultos, gbc_lbl_adultos);
+		 pn_datoBoleto.add(sp_adultos, gbc_sp_adultos);
+		 pn_datoBoleto.add(btn_selecionAsiento, gbc_btn_selecionAsiento);
+		 pn_datoBoleto.add(lbl_contador, gbc_lbl_contador);
+		 pn_datoBoleto.add(lbl_subtotal, gbc_lbl_subtotal);
+		 pn_datoBoleto.add(lbl_valor_Subtotal, gbc_lbl_valor_Subtotal);
+		 pn_datoBoleto.add(lbl_iva, gbc_lbl_iva);
+		 pn_datoBoleto.add(lbl_valor_iva, gbc_lbl_valor_iva);
+		 pn_datoBoleto.add(lbl_total, gbc_lbl_total);
+		 pn_datoBoleto.add(lbl_valor_total, gbc_lbl_valor_total);
+		 pn_compra.add(pn_controlBotones);
+		 pn_controlBotones.add(btn_anterior);
+		 pn_controlBotones.add(btn_siguiente);
 	}
 }
