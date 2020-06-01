bc2719a1877be8fa06d99d4bb90f08f2c62c4607
kevin <kevin@LAPTOP-EPM4VVHH>
2020-06-01 14:24:56 -0500

    pilas

--- src/controlador/ControladorSala.java
+++ src/controlador/ControladorSala.java
@@ -8,88 +8,84 @@ import modelo.Sala;
 import data.*;
 
 public class ControladorSala {
-    
 
-    private ArrayList<Sala> salas=new ArrayList<Sala>();
-    private Conferencias[] conferencias= Conferencias.values();
-    private Peliculas[] peliculas= Peliculas.values();
-    int numSalasPelicula=conferencias.length;
-    int numSalasConferencia=peliculas.length;
-    private Sala actSala;
-    private Stack <Integer> salaPreview = new Stack <Integer>();
-    public ControladorSala(){
-        generarSalas();
-        setSala(0);
-    }
+	private ArrayList<Sala> salas = new ArrayList<Sala>();
+	private Conferencias[] conferencias = Conferencias.values();
+	private Peliculas[] peliculas = Peliculas.values();
+	int numSalasPelicula = conferencias.length;
+	int numSalasConferencia = peliculas.length;
+	private Sala actSala;
+	private Stack<Integer> salaPreview = new Stack<Integer>();
 
-    public Sala getSala(int id){
-        return salas.get(id);
-    }
+	public ControladorSala() {
+		generarSalas();
+		setSala(0);
+	}
 
-    public void setSala(int id){
-        this.actSala=getSala(id);
-    }
+	public Sala getSala(int id) {
+		return salas.get(id);
+	}
 
-    public int getLimiteBoletos() {
-        return actSala.getAsientosDisponibles();
-    }
+	public void setSala(int id) {
+		this.actSala = getSala(id);
+	}
 
-    public void restarAsientosDisponibles(){
-        actSala.restarAsientosDisponibles();
-    }
+	public int getLimiteBoletos() {
+		return actSala.getAsientosDisponibles();
+	}
 
-    public void generarSalas() {
-        int cont=0;
+	public void restarAsientosDisponibles() {
+		actSala.restarAsientosDisponibles();
+	}
 
-        for (int i = 0; i < numSalasPelicula; i++) {
-            Peliculas pelicula= peliculas[i];
-            //for (int j = 0; j < peliculas.horario; j++) {
-                salas.add(new Sala(cont, pelicula.getSalaTipo(),calcDimencion(5,8),calcDimencion(5,8)));
-            //}
-            cont++;
-        }
+	public void generarSalas() {
+		int cont = 0;
 
-        for (int i = 0; i < numSalasConferencia; i++) {
-            Conferencias conferencia= conferencias[i];
-            salas.add(new Sala(cont, conferencia.getSalaTipo(),calcDimencion(5,8),calcDimencion(5,8)));
-            cont++;
-        }
-        cont--;
-    }
-    
-    public void anadirSalaPre(int valorx, int valory) {
-    	salaPreview.push(valorx);
-    	salaPreview.push(valory);
-    }
-    
-    public Stack<Integer> getSalaPre(){
-    	return salaPreview;
-    }
-    
-    
-    public void vaciarSeleccionados() {
-    	
-    	boolean salaPre [][] = actSala.getAsientos();
-    	
-			    	int columna = salaPreview.pop();
-			    	int fila = salaPreview.pop();
-			    	salaPre[fila][columna]=false;
-			
-		
-    
-    }
-    
-    public void ocuparAsiento(int id,int numFil,int numCol){
-        salas.get(id).ocuparAsiento(numFil,numCol);
-    }
+		for (int i = 0; i < numSalasPelicula; i++) {
+			Peliculas pelicula = peliculas[i];
+			// for (int j = 0; j < peliculas.horario; j++) {
+			salas.add(new Sala(cont, pelicula.getSalaTipo(), calcDimencion(5, 8), calcDimencion(5, 8)));
+			// }
+			cont++;
+		}
 
-    public void vaciarAsiento(int id,int numFil,int numCol){
-        salas.get(id).vaciarAsiento(numFil,numCol);
-    }
+		for (int i = 0; i < numSalasConferencia; i++) {
+			Conferencias conferencia = conferencias[i];
+			salas.add(new Sala(cont, conferencia.getSalaTipo(), calcDimencion(5, 8), calcDimencion(5, 8)));
+			cont++;
+		}
+		cont--;
+	}
 
-    private int calcDimencion(int min, int max){
-		return (int)Math.floor(Math.random() * (max - min) + min);
-    }
-    //getNumSala () numero de la sala de la pelicula
+	public void anadirSalaPre(int valorx, int valory) {
+		salaPreview.push(valorx);
+		salaPreview.push(valory);
+	}
+
+	public Stack<Integer> getSalaPre() {
+		return salaPreview;
+	}
+
+	public void vaciarSeleccionados() {
+
+		boolean salaPre[][] = actSala.getAsientos();
+
+		salaPreview.pop();
+		salaPreview.pop();
+
+	}
+
+	public void ocuparAsiento(int id, int numFil, int numCol) {
+		salas.get(id).ocuparAsiento(numFil, numCol);
+	}
+
+	public void vaciarAsiento(int id, int numFil, int numCol) {
+		salas.get(id).vaciarAsiento(numFil, numCol);
+	}
+
+	private int calcDimencion(int min, int max) {
+		return (int) Math.floor(Math.random() * (max - min) + min);
+	}
+	// getNumSala () numero de la sala de la pelicula
 
 }
